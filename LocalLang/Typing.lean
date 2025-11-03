471e415a1d1c3a06dd3877db083317c059f0410d
YuliaKorolenko <korollenkoyullia@gmail.com>
2025-11-03 19:05:53 +0100

    TypeJdgList was added

--- LocalLang/Typing.lean
+++ LocalLang/Typing.lean
@@ -3,10 +3,21 @@ import LocalLang.Types
 
 -- in context Γ expression ε has Type T
 -- TODO: define
-inductive TypeJdg : TypeContext → Expr → LLType → Prop where
-  | Jdg_Var {Γ : TypeContext}  {name : String} (H : Γ.find? (·.1 = name) = some ⟨name, LLType.nat⟩) : TypeJdg Γ (.var name) LLType.nat
-  | Jdg_Const {Γ : TypeContext} {n : ℕ} : TypeJdg Γ (.const n) LLType.nat
-  | JdgFun {Γ : TypeContext} {es : List Expr} {arg_types : List LLType} {T_return : LLType}
-                (H_f : Γ.find? (·.1 = f) = some ⟨f, .func arg_types T_return⟩)
-                (H_args : List.zip es arg_types) -- I want to declare, ∀ e : es, type : arg_types -> TypeJdg Γ e type
-                    : TypeJdg Γ (.funCall f es) (LLType.func arg_types T_return)
+
+mutual
+  inductive TypeJdg : TypeContext → Expr → LLType → Prop where
+    | Jdg_Var {Γ : TypeContext}  {name : String}
+              (H : Γ.find? (·.1 = name) = some ⟨name, LLType.nat⟩)
+                      : TypeJdg Γ (.var name) LLType.nat
+    | Jdg_Const {Γ : TypeContext} {n : ℕ} : TypeJdg Γ (.const n) LLType.nat
+    | JdgFun {Γ : TypeContext} {es : List Expr} {arg_types : List LLType} {T_return : LLType}
+                  (H_f : Γ.find? (·.1 = f) = some ⟨f, .func arg_types T_return⟩)
+                  (H_args : TypeJdgList Γ es arg_types)
+                        : TypeJdg Γ (.funCall f es) (LLType.func arg_types T_return)
+
+  inductive TypeJdgList : TypeContext -> List Expr -> List LLType -> Prop
+    | nil : TypeJdgList Γ [] []
+    | cons {e : Expr} {T : LLType} {es : List Expr} {Ts : List LLType}
+          (h_e : TypeJdg Γ e T) (h_es : TypeJdgList Γ es Ts)
+          : TypeJdgList Γ (e :: es) (T :: Ts)
+end
