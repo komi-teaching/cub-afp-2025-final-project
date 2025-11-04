d336c9ce08de7193ecdb0e57f1a53e569364c65a
Komi Golov <komi.golov@jetbrains.com>
2025-11-04 18:07:05 +0100

    Fix AST issues discussed in meeting

--- LocalLang/AST.lean
+++ LocalLang/AST.lean
@@ -1,5 +1,5 @@
 import Mathlib.Data.Nat.Notation
-
+import Std.Data.HashMap.Basic
 
 inductive BinOp where
   | add
@@ -9,13 +9,13 @@ inductive Expr where
   | const (n : ℕ)
   | var (name : String)
   | binOp (op : BinOp) (e₁ e₂ : Expr)
-  | funCall (ef : Expr) (es : List Expr)
+  | funCall (f : String) (es : List Expr)
 
 structure Function where
   parameters : List String
   body : Expr
 
-def Definitions := List Function
+abbrev Definitions := Std.HashMap String Function
 
 structure Program where
   definitions : Definitions
