4a684fd9a9baa3530f3f4f7c90f25410d549074c
Daoortor <david.dshvsk@gmail.com>
2025-11-04 16:56:39 +0100

    some fixes

--- LocalLang/Ctx.lean
+++ LocalLang/Ctx.lean
@@ -2,23 +2,12 @@ import LocalLang.AST
 
 inductive Ctx : Type where
   | hole : Ctx
-  | binOpLhs : Ctx → BinOp → Expr → Ctx
-  | binOpRhs : ℕ → BinOp → Ctx → Ctx
-  | letInExpr : String → Ctx → Expr → Ctx
-  | letInBody : String → ℕ → Ctx → Ctx
-
-abbrev Env := Std.HashMap String ℕ
+  | bin_op_lhs : Ctx → BinOp → Expr → Ctx
+  | bin_op_rhs : ℕ → BinOp → Ctx → Ctx
+  | letin : String → Ctx → Expr → Ctx
 
 @[reducible] def Ctx.fill (e : Expr) : Ctx → Expr
   | hole => e
-  | binOpLhs ctx op e' => Expr.binOp op (ctx.fill e) e'
-  | binOpRhs n op ctx => Expr.binOp op (.const n) (ctx.fill e)
-  | letInExpr x ctx e' => .letIn x (ctx.fill e) e'
-  | letInBody x n ctx => .letIn x (.const n) (ctx.fill e)
-
-@[reducible] def Ctx.updateEnv (env : Env) : Ctx → Env
-  | hole => env
-  | binOpLhs ctx _ _ => ctx.updateEnv env
-  | binOpRhs _ _ ctx => ctx.updateEnv env
-  | letInExpr _ ctx _ => ctx.updateEnv env
-  | letInBody x n ctx => ctx.updateEnv (env.insert x n)
+  | bin_op_lhs ctx op e' => Expr.binOp op (ctx.fill e) e'
+  | bin_op_rhs n op ctx => Expr.binOp op (.const n) (ctx.fill e)
+  | letin x ctx e' => .letIn x (ctx.fill e) e'
