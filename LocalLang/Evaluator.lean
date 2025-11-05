0775c27c814ff336e00e8ec12d6c42c287ad2b89
Veronika Sirotkina <veronika.sirotkina@jetbrains.com>
2025-11-05 19:39:41 +0100

    Total evaluator

--- LocalLang/Evaluator.lean
+++ LocalLang/Evaluator.lean
@@ -1,10 +1,64 @@
 import LocalLang.AST
-import Mathlib.Control.Traversable.Basic
+import Std.Data.HashMap.Basic
+import Init.Data.Repr
+
+abbrev Vars := Std.HashMap String ℕ
+
+inductive Computation (α : Type) where
+  | result (val : α) : Computation α
+  | fail : Computation α
+  | outOfGas : Computation α
+deriving Repr
+
+def Computation.bind {α β} (c : Computation α) (f : α → Computation β) : Computation β :=
+  match c with
+  | Computation.result a => f a
+  | Computation.fail  => Computation.fail
+  | Computation.outOfGas => Computation.outOfGas
+
+instance : Monad Computation where
+  pure := Computation.result
+  bind := Computation.bind
 
 def BinOp.eval : BinOp → ℕ → ℕ → ℕ
   | add, x, y => x + y
   | mul, x, y => x * y
 
-partial def Expr.eval (V : Env) : Expr → Option Value := sorry
+def Function.bindArgs (func : Func) (args : List ℕ) (V : Vars) : Vars :=
+  let param_arg_list := List.zip func.parameters args
+  param_arg_list.foldl (fun acc (name, value) => acc.insert name value) V
 
-partial def evaluate (prog : Program) : Option Value := prog.main.eval prog.env
+def Expr.eval (gas : ℕ) (V : Vars) (D : Definitions) (e : Expr) : Computation ℕ :=
+  match gas with
+  | 0 => Computation.outOfGas
+  | gas' + 1 =>
+    match e with
+    | const k =>
+        return k
+
+    | var x =>
+        match V[x]? with
+        | some v => return v
+        | none   => Computation.fail
+
+    | binOp op e₁ e₂ => do
+        let v₁ ← e₁.eval gas' V D
+        let v₂ ← e₂.eval gas' V D
+        return (op.eval v₁ v₂)
+
+    | letIn varName init expr => do
+        let varValue ← init.eval gas' V D
+        let V' := V.insert varName varValue
+        expr.eval gas' V' D
+
+    | funCall ef es => do
+        let args ← es.mapM (fun e => e.eval gas' V D)
+        match D[ef]? with
+        | none => Computation.fail
+        | some func =>
+            let V_with_args := Function.bindArgs func args V
+            -- Crucial: We call recursively with `gas'`, which is strictly smaller than `gas`.
+            func.body.eval gas' V_with_args D
+
+def Program.evaluate (prog : Program) (fuel : ℕ) : Computation ℕ :=
+  prog.main.eval fuel Std.HashMap.emptyWithCapacity prog.definitions
