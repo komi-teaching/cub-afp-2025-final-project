44e82056d1edfd8cd64df1b5a6b52d80c08162c8
YuliaKorolenko <korollenkoyullia@gmail.com>
2025-11-10 17:01:32 +0100

    TypeJdg definition (#1)
    * TypeJdg definition
    
    * TypeJdgList was added
    
    * Add examples for TypeJdg

--- LocalLang/Typing.lean
+++ LocalLang/Typing.lean
@@ -1,7 +1,21 @@
 import LocalLang.AST
 import LocalLang.Types
 
--- TODO: define
-inductive TypeJdg : TypeContext → Expr → LLType → Prop where
+-- in context Γ expression ε has Type T
 
+mutual
+  inductive TypeJdg : TypeContext → Expr → LLType → Prop where
+    | jdgVar {Γ : TypeContext}  {name : String} (H : (name, LLType.nat) ∈ Γ)
+                      : TypeJdg Γ (.var name) LLType.nat
+    | jdgConst {Γ : TypeContext} {n : ℕ} : TypeJdg Γ (.const n) LLType.nat
+    | jdgFun {Γ : TypeContext} {es : List Expr} {arg_types : List LLType} {T_return : LLType}
+                  (H_f : (f, .func arg_types T_return) ∈ Γ)
+                  (H_args : TypeJdgList Γ es arg_types)
+                        : TypeJdg Γ (.funCall f es) (LLType.func arg_types T_return)
 
+  inductive TypeJdgList : TypeContext -> List Expr -> List LLType -> Prop
+    | nil : TypeJdgList Γ [] []
+    | cons {e : Expr} {T : LLType} {es : List Expr} {Ts : List LLType}
+          (h_e : TypeJdg Γ e T) (h_es : TypeJdgList Γ es Ts)
+          : TypeJdgList Γ (e :: es) (T :: Ts)
+end
