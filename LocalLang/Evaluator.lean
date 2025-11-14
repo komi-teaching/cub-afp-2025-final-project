efdc7d7e51dbe4308558def97445adea65ce680b
Daoortor <david.dshvsk@gmail.com>
2025-11-14 23:36:50 +0100

    some preliminary fixes to ensure that it builds

--- LocalLang/Evaluator.lean
+++ LocalLang/Evaluator.lean
@@ -1,39 +1,43 @@
 import LocalLang.AST
 
--- def Vars := Std.HashMap String ℕ
+abbrev Vars := Std.HashMap String ℕ
 
 def BinOp.eval : BinOp → ℕ → ℕ → ℕ
   | add, x, y => x + y
   | mul, x, y => x * y
 
-def Function.inline (V : Std.HashMap String ℕ) : Function → List ℕ → (Std.HashMap String ℕ × Expr)
+def Function.inline (V : Vars) : Function → List ℕ → (Std.HashMap String ℕ × Expr)
   | func, agrs =>
     let param_arg_list := List.zip func.parameters agrs
     let V_with_args := param_arg_list.foldl
       (fun acc (name, value) => acc.insert name value) V
     (V_with_args, func.body)
 
-def List.nonnull : List (Option ℕ) → Option (List ℕ)
+def List.sequence : List (Option ℕ) → Option (List ℕ)
   | [] => some []
   | none :: _ => none
   | (some x) :: xs => do
-    let ys <- List.nonnull xs
+    let ys <- List.sequence xs
     some (x :: ys)
 
-partial def Expr.eval (V : Std.HashMap String ℕ) : Expr → Option ℕ
+partial def Expr.eval (V : Vars) (D : Definitions) : Expr → Option ℕ
   | const k => pure k
   | var x => V[x]?
   | binOp op e₁ e₂ => do
-      let v₁ <- e₁.eval V
-      let v₂ <- e₂.eval V
+      let v₁ <- e₁.eval V D
+      let v₂ <- e₂.eval V D
       pure (op.eval v₁ v₂)
   | funCall ef es => do
-    let args_opt : List (Option ℕ) := es.map (fun (elm) => elm.eval V)
-    let args : List ℕ <- args_opt.nonnull
-    let (V_with_args, expr) := Function.inline V ef args
-    let evaled := expr.eval V_with_args
+    let args_opt : List (Option ℕ) := es.map (fun (elm) => elm.eval V D)
+    let args : List ℕ <- args_opt.sequence
+    let func <- D[ef]?
+    let (V_with_args, expr) := Function.inline V func args
+    let evaled := expr.eval V_with_args D
     evaled
+  | letIn x e body => do
+    let vₓ <- e.eval V D
+    let result <- body.eval (V.insert x vₓ) D
+    result
 
--- Maybe `m ℕ` for some monad `m`?
--- TODO: implement
-partial def evaluate : Program → ℕ := sorry
+partial def evaluate : Program → Option ℕ
+  | prog => prog.main.eval Std.HashMap.emptyWithCapacity prog.definitions
