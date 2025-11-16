a12e61417f28ca144473527295b5c0a626cdd5f8
Daoortor <34247454+Daoortor@users.noreply.github.com>
2025-11-16 16:25:55 +0100

    Prove confluence for SmallSteps (#16)
    Prove determinism for SmallStep and confluence for SmallSteps, along with some lemmas for semantics

--- LocalLang/Ctx.lean
+++ LocalLang/Ctx.lean
@@ -22,3 +22,10 @@ abbrev Env := Std.HashMap String ℕ
   | binOpRhs _ _ ctx => ctx.updateEnv env
   | letInExpr _ ctx _ => ctx.updateEnv env
   | letInBody x n ctx => ctx.updateEnv (env.insert x n)
+
+@[reducible] def Ctx.fillWithCtx (ctx : Ctx) (innerCtx : Ctx) : Ctx := match ctx with
+  | .hole => innerCtx
+  | .binOpLhs ctx₀ op e => .binOpLhs (Ctx.fillWithCtx ctx₀ innerCtx) op e
+  | .binOpRhs n op ctx₀ => .binOpRhs n op (Ctx.fillWithCtx ctx₀ innerCtx)
+  | .letInExpr x ctx₀ e => .letInExpr x (Ctx.fillWithCtx ctx₀ innerCtx) e
+  | .letInBody x n ctx₀ => .letInBody x n (Ctx.fillWithCtx ctx₀ innerCtx)
