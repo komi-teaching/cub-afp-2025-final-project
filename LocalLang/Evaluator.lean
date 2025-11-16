d07e421748daa821995840be8d972225fc894ad9
Daoortor <david.dshvsk@gmail.com>
2025-11-16 17:05:43 +0100

    fixes

--- LocalLang/Evaluator.lean
+++ LocalLang/Evaluator.lean
@@ -9,6 +9,20 @@ def Function.updateEnv (func : Function) (V : Env) (args : List ℕ) : Env :=
   let param_arg_list := List.zip func.parameters args
   V ∪ .ofList param_arg_list
 
+-- test for updateEnv, ensures that existing values get overwritten correctly
+example :=
+  let V : Env := .ofList [("x", 1)]
+  let func : Function := ⟨["x"], .const 0⟩
+  let V' := Function.updateEnv func V [2]
+  let result : V'["x"]? = some 2 := by
+    unfold V'
+    unfold V
+    unfold func
+    rw [Function.updateEnv, List.zip, List.zipWith, ← Std.HashMap.get?_eq_getElem?,
+      Std.HashMap.get?_union]
+    simp
+  result
+
 partial def Expr.eval (V : Env) (D : Definitions) : Expr → Option ℕ
   | const k => pure k
   | var x => V[x]?
@@ -18,7 +32,7 @@ partial def Expr.eval (V : Env) (D : Definitions) : Expr → Option ℕ
     pure (op.eval v₁ v₂)
   | funCall ef es => do
     let func <- D[ef]?
-    let args_opt : List (Option ℕ) := es.map (fun (elm) => elm.eval V D)
+    let args_opt : List (Option ℕ) := es.map (·.eval V D)
     let args : List ℕ <- sequence args_opt
     let V_with_args := Function.updateEnv func V args
     func.body.eval V_with_args D
