122d2c10d1091b1e4efb8892b8d86b2474e2dada
Daoortor <david.dshvsk@gmail.com>
2025-11-18 21:00:27 +0100

    add example

--- LocalLang/EvaluatorCorrectness.lean
+++ LocalLang/EvaluatorCorrectness.lean
@@ -2,7 +2,19 @@ import LocalLang.AST
 import LocalLang.Evaluator
 import LocalLang.Semantics
 
--- lemma smallStep_preserves_eval
+example {defs : Definitions} : V[x]? = some n → Expr.eval V defs (.var x) = some n := by
+  intro h_in
+  -- unfold Expr.eval     fails
+  sorry
+
+lemma headSmallStep_preserves_eval {defs : Definitions} : e₁.eval V defs = some n →
+  HeadSmallStep defs V e₁ e₂ → e₂.eval V defs = some n := by
+  intro e₁_eval_eq hstep
+  cases hstep with
+  | var_step h_in => sorry
+  | bin_op_step => sorry
+  | let_in_const_step => sorry
+  | fun_step => sorry
 
 -- TODO: eval e = k iff e steps to k
 lemma eval_eq_some_of_smallSteps {defs : Definitions} : SmallSteps defs V e (.const n) →
