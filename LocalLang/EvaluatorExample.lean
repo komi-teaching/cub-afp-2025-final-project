e9d376df7f414e6399ba6009bda4649b5d63b118
Veronika Sirotkina <veronika.sirotkina@jetbrains.com>
2025-12-09 13:31:44 +0100

    fixup! Evaluator usage example

--- LocalLang/EvaluatorExample.lean
+++ LocalLang/EvaluatorExample.lean
@@ -1,20 +1,18 @@
 import LocalLang.Evaluator
 
-private def loopFunc : Func := {
-  parameters := ["x"],
-  body := Expr.funCall "loop" [Expr.var "x"]
-}
+private def loopFuncBody : Expr := Expr.funCall (Expr.var "loop") [Expr.var "x"]
+private def loopFunc : Value := Value.closure ["x"] loopFuncBody
 
 private def loopProg : Program := {
-  definitions := Std.HashMap.emptyWithCapacity.insert "loop" loopFunc,
-  main := Expr.funCall "loop" [Expr.const 1]
+  env := Std.HashMap.emptyWithCapacity.insert "loop" loopFunc,
+  main := Expr.funCall (Expr.value loopFunc) [Expr.const 1]
 }
 
 -- Returns: Computation.outOfGas
 #eval loopProg.evaluate 100
 
 def constProg : Program := {
-  definitions := Std.HashMap.emptyWithCapacity,
+  env := Std.HashMap.emptyWithCapacity,
   main := Expr.const 1
 }
 
