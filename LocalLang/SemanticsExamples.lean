038bc1fde59082717a590b5419b5226d04b1a914
markthehopeful <markthehopeful@gmail.com>
2025-12-22 14:13:15 +0100

    Removed all "ofNat" mentions, fixed the example accordingly

--- LocalLang/SemanticsExamples.lean
+++ LocalLang/SemanticsExamples.lean
@@ -12,10 +12,10 @@ instance : Add Expr where
 instance : Coe String Expr where
   coe := .var
 
-instance : OfNat Expr n where
-  ofNat := .value (.nat n)
+--instance : OfNat Expr n where
+--  ofNat := .value (.nat n)
 
-abbrev f_body : Expr := (.funCall "g" [ "x" + 1 ]) + "x"
+abbrev f_body : Expr := (.funCall "g" [ "x" + (.value (.nat 1)) ]) + "x"
 abbrev f : Value := .closure ["x"] f_body
 abbrev g : Value := .closure ["x"] "x"
 
@@ -28,27 +28,27 @@ abbrev defs := Std.HashMap.ofList [("f", f), ("g", g)]
 
 lemma f_steps
   : SmallSteps (defs.insert "x" 0)
-      (Expr.funCall "g" ["x" + 1] + Expr.var "x") 1 := by
+      (Expr.funCall "g" ["x" + (.value (.nat 1))] + Expr.var "x") (.value (.nat 1)) := by
   let env := defs.insert "x" 0
   calc
-    SmallSteps env _ (1 + "x") := by
+    SmallSteps env _ ((.value (.nat 1)) + "x") := by
       apply (SmallSteps.with_ctx (.binOpLhs .hole .add "x") rfl rfl)
       simp only [Ctx.updateEnv]
       calc
-        SmallStep env _ (.funCall (.value (.closure ["x"] "x")) ["x" + 1]) := by
+        SmallStep env _ (.funCall (.value (.closure ["x"] "x")) ["x" + (.value (.nat 1))]) := by
           step_auto_context
           apply HeadSmallStep.var_step
           simp [env, Std.HashMap.getElem_insert]
-        SmallStep env _ (.letIn "x" ("x" + 1) "x") := by
+        SmallStep env _ (.letIn "x" ("x" + (.value (.nat 1))) "x") := by
           step_auto_context
           apply (HeadSmallStep.fun_step ?_ rfl)
           try rw [defs_g]
           simp
-        SmallSteps env _ (.letIn "x" 1 "x") := by
+        SmallSteps env _ (.letIn "x" (.value (.nat 1)) "x") := by
           apply (SmallSteps.with_ctx (.letInExpr "x" .hole "x") rfl rfl)
           simp only [Ctx.updateEnv]
           calc
-            SmallStep env _ (0 + 1) := by
+            SmallStep env _ ((.value (.nat 0)) + (.value (.nat 1))) := by
               step_auto_context
               apply HeadSmallStep.var_step
               simp [Ctx.updateEnv, env]
@@ -57,31 +57,39 @@ lemma f_steps
               step_auto_context
               apply HeadSmallStep.bin_op_step
               constructor
-        SmallStep env _ (.letIn "x" 1 1) := by
+        SmallStep env _ (.letIn "x" (.value (.nat 1)) (.value (.nat 1))) := by
+          apply SmallStep.with_ctx (.letInBody "x" 1 .hole) (by simp; rfl) (by simp; rfl)
+            (?_ : SmallStep _ "x" (.value (.nat 1)))
           step_auto_context
-        SmallStep env _ 1 := by
+          apply HeadSmallStep.var_step
+          rw [Std.HashMap.getElem?_insert_self]
+          rfl
+        SmallStep env _ (.value (.nat 1)) := by
           step_auto_context
           constructor
-    SmallStep env _ (1 + 0) := by
+    SmallStep env _ ((.value (.nat 1)) + (.value (.nat 0))) := by
       step_auto_context
+      constructor
+      simp [env]
+      rfl
     SmallStep env _ _ := by
       step_auto_context
       constructor
       rfl
 
-example : SmallSteps defs (.funCall "f" [0]) 1 := by
+example : SmallSteps defs (.funCall "f" [(.value (.nat 0))]) (.value (.nat 1)) := by
   calc
-    SmallStep defs _ (.funCall (.value (.closure ["x"] f_body)) [0]) := by
-      apply SmallStep.ctx_step (.funCallBody .hole [0]) (by simp) (by simp)
+    SmallStep defs _ (.funCall (.value (.closure ["x"] f_body)) [(.value (.nat 0))]) := by
+      apply SmallStep.ctx_step (.funCallBody .hole [(.value (.nat 0))]) (by simp) (by simp)
         (?_ : HeadSmallStep _ "f" (.value (.closure ["x"] f_body)))
       constructor
       simp
-    SmallStep defs _ (.letIn "x" 0 f_body) := by
+    SmallStep defs _ (.letIn "x" (.value (.nat 0)) f_body) := by
       apply SmallStep.hole_step
       apply (HeadSmallStep.fun_step ?_ rfl)
       try rw [defs_f]
       simp
-    SmallSteps defs _ (.letIn "x" 0 1) := by
+    SmallSteps defs _ (.letIn "x" (.value (.nat 0)) (.value (.nat 1))) := by
       apply (SmallSteps.with_ctx (.letInBody "x" 0 .hole) rfl rfl)
       exact f_steps
     SmallStep defs _ _ := by
